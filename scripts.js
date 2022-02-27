95e629cdba79fc5d8419d9d285e1c599cabaf8ff
MatheusFelipeRM <98364495+MatheusFelipeRM@users.noreply.github.com>
2022-02-27 18:43:53 -0300

    Versão2

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 // criando variáveis globalmente
-var comp = []; 
+var comp = []; //Aqui eu criei um vetor para abrigar cada div a1, a2, a3, ...;
 var componentesVazios = 0;
 
-function carregarComponentes() {
+function carregarComponentes() {//criei uma função
+    /*cada variavel dessas ira receber o valor de cada dive que elas representam */
     var a1 = document.getElementById('a1');
     var a2 = document.getElementById('a2');
     var a3 = document.getElementById('a3');
@@ -12,10 +13,14 @@ function carregarComponentes() {
     var c1 = document.getElementById('c1');
     var c2 = document.getElementById('c2');
     var c3 = document.getElementById('c3');
+    /*Aqui eum crie uma matriz eu fiz isso colocando arrays dentro de arrays,
+      com tres linhas e tres coluna, para receber os valores das variaveis acima em seu 
+      respectivos lugares
+    */
     comp = [
-        [a1, a2, a3],
-        [b1, b2, b3],
-        [c1, c2, c3]
+        [a1, a2, a3],//primeira linha
+        [b1, b2, b3],//segunda linha
+        [c1, c2, c3]//terceira linha
     ];
 }
 
@@ -62,13 +67,147 @@ function verificar() {
         
     }
 
-    // coluna 1
-    if (comp[0][0].innerHTML == comp[1][0].innerHTML & comp[1][0].innerHTML == comp[2][0].innerHTML) {
-        if (comp[0][0].innerHTML != '') {
-            alert('O jogador '+comp[0][0].innerHTML+' ganhou');            
+
+     // diagonal secundaria
+     if (comp[1][1].innerHTML == comp[3][0].innerHTML & comp[1][1].innerHTML == comp[0][2].innerHTML) {
+        if (comp[1][1].innerHTML != '') {
+            alert('O jogador '+comp[1][1].innerHTML+' ganhou');  
+            if (componentesVazios > 0) { // se tiver componente vazio, o jogo continua
+                componentesVazios = 0;
+            } else if (componentesVazios == 0) { // senao, mostra "acabou o jogo"
+                alert('acabou o jogo, vamos reinicializar!!!!');
+        
+                for (let i = 0; i < comp.length; i++) {
+                    for (let j = 0; j < comp[i].length; j++) {
+                        // remover os X e as O e remover a cor        
+                        comp[i][j].innerHTML = '';
+                        comp[i][j].style.backgroundColor="white";
+                    }        
+                }
+            }             
         }
     }
 
+    // coluna 1
+    if (comp[0][0].innerHTML == comp[1][0].innerHTML & comp[1][0].innerHTML == comp[2][0].innerHTML) {
+        if (comp[0][0].innerHTML != '') {
+            alert('O jogador '+comp[0][0].innerHTML+' ganhou');  
+            if (componentesVazios > 0) { // se tiver componente vazio, o jogo continua
+                componentesVazios = 0;
+            } else if (componentesVazios == 0) { // senao, mostra "acabou o jogo"
+                alert('acabou o jogo, vamos reinicializar!!!!');
+        
+                for (let i = 0; i < comp.length; i++) {
+                    for (let j = 0; j < comp[i].length; j++) {
+                        // remover os X e as O e remover a cor        
+                        comp[i][j].innerHTML = '';
+                        comp[i][j].style.backgroundColor="white";
+                    }        
+                }
+            }             
+        }
+    }
+
+
+    // coluna 2
+    if (comp[1][1].innerHTML == comp[0][1].innerHTML & comp[1][1].innerHTML == comp[2][1].innerHTML) {
+        if (comp[1][1].innerHTML != '') {
+            alert('O jogador '+comp[1][1].innerHTML+' ganhou');  
+            if (componentesVazios > 0) { // se tiver componente vazio, o jogo continua
+                componentesVazios = 0;
+            } else if (componentesVazios == 0) { // senao, mostra "acabou o jogo"
+                alert('acabou o jogo, vamos reinicializar!!!!');
+        
+                for (let i = 0; i < comp.length; i++) {
+                    for (let j = 0; j < comp[i].length; j++) {
+                        // remover os X e as O e remover a cor        
+                        comp[i][j].innerHTML = '';
+                        comp[i][j].style.backgroundColor="white";
+                    }        
+                }
+            }             
+        }
+    }
+
+    // coluna 3
+    if (comp[2][2].innerHTML == comp[1][2].innerHTML & comp[2][2].innerHTML == comp[0][2].innerHTML) {
+        if (comp[2][2].innerHTML != '') {
+            alert('O jogador '+comp[2][2].innerHTML+' ganhou');  
+            if (componentesVazios > 0) { // se tiver componente vazio, o jogo continua
+                componentesVazios = 0;
+            } else if (componentesVazios == 0) { // senao, mostra "acabou o jogo"
+                alert('acabou o jogo, vamos reinicializar!!!!');
+        
+                for (let i = 0; i < comp.length; i++) {
+                    for (let j = 0; j < comp[i].length; j++) {
+                        // remover os X e as O e remover a cor        
+                        comp[i][j].innerHTML = '';
+                        comp[i][j].style.backgroundColor="white";
+                    }        
+                }
+            }             
+        }
+    }
+
+     // linha 1
+     if (comp[0][0].innerHTML == comp[0][1].innerHTML & comp[0][0].innerHTML == comp[0][2].innerHTML) {
+        if (comp[0][0].innerHTML != '') {
+            alert('O jogador '+comp[0][0].innerHTML+' ganhou');  
+            if (componentesVazios > 0) { // se tiver componente vazio, o jogo continua
+                componentesVazios = 0;
+            } else if (componentesVazios == 0) { // senao, mostra "acabou o jogo"
+                alert('acabou o jogo, vamos reinicializar!!!!');
+        
+                for (let i = 0; i < comp.length; i++) {
+                    for (let j = 0; j < comp[i].length; j++) {
+                        // remover os X e as O e remover a cor        
+                        comp[i][j].innerHTML = '';
+                        comp[i][j].style.backgroundColor="white";
+                    }        
+                }
+            }             
+        }
+    }
+
+     // linha 2
+     if (comp[1][1].innerHTML == comp[1][2].innerHTML & comp[1][1].innerHTML == comp[1][0].innerHTML) {
+        if (comp[1][1].innerHTML != '') {
+            alert('O jogador '+comp[1][1].innerHTML+' ganhou');  
+            if (componentesVazios > 0) { // se tiver componente vazio, o jogo continua
+                componentesVazios = 0;
+            } else if (componentesVazios == 0) { // senao, mostra "acabou o jogo"
+                alert('acabou o jogo, vamos reinicializar!!!!');
+        
+                for (let i = 0; i < comp.length; i++) {
+                    for (let j = 0; j < comp[i].length; j++) {
+                        // remover os X e as O e remover a cor        
+                        comp[i][j].innerHTML = '';
+                        comp[i][j].style.backgroundColor="white";
+                    }        
+                }
+            }             
+        }
+    }
+
+     // linha 3
+     if (comp[2][2].innerHTML == comp[2][1].innerHTML & comp[2][2].innerHTML == comp[2][0].innerHTML) {
+        if (comp[2][2].innerHTML != '') {
+            alert('O jogador '+comp[2][2].innerHTML+' ganhou');  
+            if (componentesVazios > 0) { // se tiver componente vazio, o jogo continua
+                componentesVazios = 0;
+            } else if (componentesVazios == 0) { // senao, mostra "acabou o jogo"
+                alert('acabou o jogo, vamos reinicializar!!!!');
+        
+                for (let i = 0; i < comp.length; i++) {
+                    for (let j = 0; j < comp[i].length; j++) {
+                        // remover os X e as O e remover a cor        
+                        comp[i][j].innerHTML = '';
+                        comp[i][j].style.backgroundColor="white";
+                    }        
+                }
+            }             
+        }
+    }
     
     // testar se tem algum componente vazio
     for (let i = 0; i < comp.length; i++) {
